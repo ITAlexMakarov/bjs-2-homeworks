11bba59f21404d3a2c702719c884c69a936700a8
itAleks <it.aleksmakarov@gmail.com>
2023-04-29 14:49:47 +0300

    Added an experts solution

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -7,6 +7,11 @@ function compareArrays(arr1, arr2) {
 Массивы будут одинаковыми, если длины массивов будут одинаковыми И каждый элемент будет совпадать с соответствующим 
 элементом другого массива: return arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index]);*/
 
+/*Решение эксперта
+function compareArrays(arr1, arr2) {
+    return arr1.every((el, idx) => arr1.length === arr2.length && el === arr2[idx]);  
+  }*/
+
 // Задание 2
 function getUsersNamesInAgeRange(users, gender) {
     let result = users.filter(user => user.gender === gender).map(user => user.age).reduce((acc, item, index, arr) =>  {
@@ -21,3 +26,11 @@ function getUsersNamesInAgeRange(users, gender) {
 /*Описание: новый массив с отобранным гендером, используя метод перебора массива filter,
 получение возраста, используя метод преобразования массива map
 вычисление среднего возраста, используя аккумулирующий метод reduce*/
+
+/*Решение эксперта
+function getUsersNamesInAgeRange(users, gender) {
+    return users.
+      filter(user => user.gender === gender).
+      map(user => user.age).
+      reduce((average, userAge, index, array) => average + userAge / array.length, 0);
+  }*/
